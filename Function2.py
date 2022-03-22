6656a89706c8b6406ffdcde8aa4c76f89d388a73
papasmf1 <papasmf1@gmail.com>
2022-03-22 16:46:17 +0900

    교집합 함수 연습

--- Function2.py
+++ Function2.py
@@ -41,3 +41,31 @@ def func2(a):
 #호출
 print(func2(1))
 
+#불변형식인 전역변수에 읽기+쓰기까지 하는 경우
+g = 1 
+def testScope(a):
+    #global g 
+    #지역변수를 초기화(우선적으로 사용)
+    g = 2 
+    return a+g 
+
+#호출
+print(testScope(1))
+print("전역변수 g:", g)
+
+
+#디버깅 연습(교집합으로 되어 있는 문자를 출력)
+def intersect(prelist, postlist):
+    #지역변수에 겹치는 글자를 모아두가 
+    retList = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        #and연산자는 ~이면서 ~이고 
+        #"A"라는 글자가 postlist에 포함되어 있고 그리고 "A"글자가 아직 없다. 
+        # in(포함되어있음) not in(포함되어있지 않다)
+        if x in postlist and x not in retList:
+            retList.append(x)
+    return retList 
+
+#호출
+print(intersect("HAM","SPAM"))
