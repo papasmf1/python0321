8128e91ec809264dd24078dbb45efa832bbf949e
papasmf1 <papasmf1@gmail.com>
2022-03-22 11:26:42 +0900

    딕셔너리 추가 데모

---
+++ DemoDict.py
@@ -0,0 +1,14 @@
+# DemoDict.py
+
+device = {"아이폰":5, "아이패드":10, "윈도우":20}
+
+for item in device.items():
+    print(item)
+
+for key in device.keys():
+    print(key)
+
+for value in device.values():
+    print(value)
+
+
