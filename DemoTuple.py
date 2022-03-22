efd542805fd8d4c9ffc3060e7115c00f7a350085
papasmf1 <papasmf1@gmail.com>
2022-03-22 10:25:12 +0900

    튜퓰을연습

--- DemoTuple.py
+++ DemoTuple.py
@@ -8,3 +8,23 @@ print(b)
 print(a.union(b))
 print(a.difference(b))
 
+
+#튜플 사용
+tp = (1,2,3)
+print(tp)
+print(len(tp))
+
+#함수를 정의
+def calc(a,b):
+    return a+b, a*b
+
+#첫번째 컬럼 이동
+#함수를 호출
+print(calc(3,4))
+print("-------")
+result = calc(5,6)
+print(result)
+print(result[0], result[1])
+
+
+
