33ac5d7efe5bf7c524f31e9e063286421814dfd7
papasmf1 <papasmf1@gmail.com>
2022-03-23 15:31:59 +0900

    모듈을 연습2

--- DemoModule.py
+++ DemoModule.py
@@ -4,3 +4,14 @@ import math
 
 print(dir(math))
 
+print(math.log(100))
+print(math.sin(100))
+print(math.cos(100))
+
+#파일 목록 가져오기
+import glob 
+print(glob.glob("c:\\work\\*.py"))
+
+import os 
+print("운영체제이름:", os.name)
+
