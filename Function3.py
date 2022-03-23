1fb2fe48ba4f00ab6cb364a840629ac5dbc346e1
papasmf1 <papasmf1@gmail.com>
2022-03-23 09:41:00 +0900

    함수연습4

--- Function3.py
+++ Function3.py
@@ -19,3 +19,27 @@ print(connectURI("credu.com","80"))
 print(connectURI(port="8080", server="credu.com"))
 
 
+#가변인자 처리
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print(union("HAM","EGG"))
+print(union("HAM","EGG","SPAM"))
+
+#정의되지 않은 인자(**, 내부에서 딕셔너리)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출
+print(userURIBuilder("credu.com","80", id="kim", passwd="1234")) 
+print(userURIBuilder("credu.com","80", id="kim", passwd="1234", age="30"))
+ 
