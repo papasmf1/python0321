08543b5906af9d7a148088e800808d3f0ba039eb
papasmf1 <papasmf1@gmail.com>
2022-03-23 11:30:09 +0900

    클래스 연습2

--- class2.py
+++ class2.py
@@ -1,19 +1,23 @@
-# class1.py 
+# class2.py 
+
 #클래스 정의
 class Person:
+    #클래스 멤버 변수(데이터 공유가 목적)
+    num_person = 0 
     #초기화 메서드(가장 먼저 수행되는 생성자 메서드)
     def __init__(self):
         #인스턴스 멤버 변수
         self.name = "default name"
+        Person.num_person += 1 
     def print(self):
         print("My name is {0}".format(self.name))
 
 #인스턴스 생성
 p1 = Person()
 p2 = Person()
-#멤버변수에 접근
-p2.name = "전우치"
-p1.print()
-p2.print()
+p3 = Person() 
+print("인스턴스의 개수:", Person.num_person)
+
+
 
     
