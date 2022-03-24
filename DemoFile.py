8e9b0d893d2fbae8864aa253aa5aef4a5f953c87
papasmf1 <papasmf1@gmail.com>
2022-03-24 10:51:58 +0900

    파일 처리

---
+++ DemoFile.py
@@ -0,0 +1,12 @@
+# DemoFile.py 
+
+import sys 
+
+print("hello", "python", sep="~", end="!", file=sys.stderr)
+
+print("hello\nworld")
+
+URL = "http://www.credu.com/?page=" + str(1) 
+
+print(URL)
+
