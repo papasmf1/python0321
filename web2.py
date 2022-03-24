6c816bf246e2643d7d624a2b77f9bb9b28da636a
papasmf1 <papasmf1@gmail.com>
2022-03-24 16:55:36 +0900

    클리앙 중고장터

--- web2.py
+++ web2.py
@@ -1,17 +1,34 @@
 # web2.py
+#웹서버와 통신 
 import urllib.request
+#크롤링 
 from bs4 import BeautifulSoup
+
 data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri')
 soup = BeautifulSoup(data, 'html.parser')
 
-cartoons = soup.find_all("td", class_="title")
+#ctrl + /: 직접 분석  
+# <td class="title">
+# 	<a href="/webtoon/detail?titleId=20853&no=50&weekday=fri">마음의 소리 50화 <격렬한 나의 하루></a>
+# </td>
 
+#<td> 10개 
+cartoons = soup.find_all("td", class_="title")
+print(len(cartoons))
+
+#슬라이싱
 title = cartoons[0].find('a').text
 link = cartoons[0].find('a')['href']
 print(title)
 print(link)
 
+#파일로 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
 for item in cartoons:
     title = item.find('a').text
     print(title)
+    f.write(title + "\n")
+
+f.close() 
+
 
--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re 
 
+#웹봇(웹로봇)
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
@@ -12,21 +13,23 @@ for n in range(0,10):
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
+
         data = urllib.request.urlopen(req).read()
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+
+        # <span class="subject_fixed" data-role="list-title-text" title="아이폰11프로 64GB 미드나잇 그린 리퍼 미사용 판매합니다.">
+        # 	아이폰11프로 64GB 미드나잇 그린 리퍼 미사용 판매합니다.
+        # </span>
+
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
-                        if (re.search('아이폰', title)):
-                                print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
+                        title = item.text
+                        print(title.strip())
+                        # if (re.search('아이폰', title)):
+                        #         print(title.strip())
                 except:
                         pass
         
