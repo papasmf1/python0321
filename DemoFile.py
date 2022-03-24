d6c28deecdc189956c30685e3acba7749d43ab4a
papasmf1 <papasmf1@gmail.com>
2022-03-24 11:07:24 +0900

    서식문자 지정

--- DemoFile.py
+++ DemoFile.py
@@ -2,11 +2,27 @@
 
 import sys 
 
-print("hello", "python", sep="~", end="!", file=sys.stderr)
+#print("hello", "python", sep="~", end="!", file=sys.stderr)
 
 print("hello\nworld")
 
 URL = "http://www.credu.com/?page=" + str(1) 
-
 print(URL)
+
+#문자열 정렬
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).rjust(3))
+
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).zfill(3))
+
+#binary(b), hex(x), c(character) ASCII코드 테이블
+print("---2진수,16진수 출력(서식문자)---")
+print("{0:b}".format(10))
+print("{0:x}".format(10))
+print("{0:c}".format(65))
+#엑셀 서식(화폐, 돈)
+print("{0:,}".format(15000))
+
+
 
