c75a3f2a4d01713179a11ad258a3f93d0ce8078e
papasmf1 <papasmf1@gmail.com>
2022-03-24 16:34:28 +0900

    네이버 웹툰 크롤링

---
+++ web2.py
@@ -0,0 +1,21 @@
+# web2.py
+
+data = urllib.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri')
+
+
+import urllib.request
+from bs4 import BeautifulSoup
+data = urllib.request.urlopen('http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri')
+soup = BeautifulSoup(data, 'html.parser')
+
+cartoons = soup.fidn_all("td", class_="title")
+
+title = cartoons[0].find('a').text
+link = cartoons[0].find('a')['href']
+print(title)
+print(link)
+
+for item in cartoons:
+    title = item.find('a').text
+    print(title)
+
