7d940c33f3318c9197ef652b616fefa7d8eace39
papasmf1 <papasmf1@gmail.com>
2022-03-24 16:31:11 +0900

    크롤링연습2

--- web1.py
+++ web1.py
@@ -1,10 +1,25 @@
 # web1.py
-
 from bs4 import BeautifulSoup
 
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+#print(soup.prettify())
+
+#<p>태그 몽땅 
+#print(soup.find_all("a"))
+
+#첫번째 <p>만 검색
+#print(soup.find("p"))
+
+#필터링: <p class="outer-text">컨텐츠</p>
+result = soup.find_all("p", class_="outer-text")
+for item in result:
+    strContent = item.text 
+    print(strContent.strip())
+
+
+
+
 
